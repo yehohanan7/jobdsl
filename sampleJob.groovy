0dbcec7b8519731f5b2201007ce61865f9790bb2
John <yehohanan7@gmail.com>
2016-08-03 14:56:05 +0530

    update

--- sampleJob.groovy
+++ sampleJob.groovy
@@ -1,6 +1,6 @@
 job("emacsd") {
     scm {
-        github https://github.com/yehohanan7/emacsd.git
+        github "https://github.com/yehohanan7/emacsd.git"
     }
     triggers {
         scm 'H/5 * * * *'
